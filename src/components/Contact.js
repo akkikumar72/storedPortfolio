74388aaef3023afeb7bb637bee9be8afaffb0034
akash.pathak <akkikumar72@gmail.com>
2020-05-12 23:01:38 +0200

    Few changes to forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,10 @@ const Contact = (props) => (
         <div className="inner">
             <section>
                 <form  id="contact-form"
-                       class="validate-form"
+                       className="validate-form"
                        method="post"
-                       name="contact"
+                       name="contact-form"
+                       data-netlify-recaptcha="true"
                        data-netlify="true">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
@@ -19,8 +20,9 @@ const Contact = (props) => (
                     </div>
                     <div className="field">
                         <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
+                        <textarea name="message" id="message" rows="6"/>
                     </div>
+                    <div data-netlify-recaptcha="true"></div>
                     <ul className="actions">
                         <li><input type="submit" value="Send Message" className="special" /></li>
                         <li><input type="reset" value="Clear" /></li>
