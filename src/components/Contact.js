198a20640a8b54cda46c8d8f98a9592b7a3a17a0
akash.pathak <akkikumar72@gmail.com>
2020-05-12 23:21:58 +0200

    Adding netlify forrms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,29 +4,44 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form  id="contact-form"
-                       className="validate-form"
-                       method="post"
-                       name="contact-form"
-                       data-netlify-recaptcha="true"
-                       data-netlify="true">
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" required/>
-                    </div>
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" required/>
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"/>
-                    </div>
-                    <div data-netlify-recaptcha="true"></div>
-                    <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
+                {/*<form  */}
+                {/*       className="validate-form"*/}
+                {/*       method="POST"*/}
+                {/*       name="contact-form"*/}
+                {/*       data-netlify-recaptcha="true"*/}
+                {/*       data-netlify="true">*/}
+                {/*    <div className="field half first">*/}
+                {/*        <label htmlFor="name">Name</label>*/}
+                {/*        <input type="text" name="name" id="name" required/>*/}
+                {/*    </div>*/}
+                {/*    <div className="field half">*/}
+                {/*        <label htmlFor="email">Email</label>*/}
+                {/*        <input type="text" name="email" id="email" required/>*/}
+                {/*    </div>*/}
+                {/*    <div className="field">*/}
+                {/*        <label htmlFor="message">Message</label>*/}
+                {/*        <textarea name="message" id="message" rows="6"/>*/}
+                {/*    </div>*/}
+                {/*    <div data-netlify-recaptcha="true"/>*/}
+                {/*    <ul className="actions">*/}
+                {/*        <li><input type="submit" value="Send Message" className="special" /></li>*/}
+                {/*        <li><input type="reset" value="Clear" /></li>*/}
+                {/*    </ul>*/}
+                {/*</form>*/}
+
+                <form name="contact" method="POST" data-netlify="true">
+                    <p>
+                        <label>Your Name: <input type="text" name="name" /></label>
+                    </p>
+                    <p>
+                        <label>Your Email: <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message" rows="6"/></label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
                 </form>
             </section>
             <section className="split">
