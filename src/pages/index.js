976118752068cf3d0593b24dce9d40f10b6760a3
Akash Kumar Pathak <akkikumar72@users.noreply.github.com>
2020-08-25 19:18:28 +0200

    Adding gtag (#28)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Layout from '../components/layout'
-import Banner from '../components/Banner'
+import React from 'react';
+import Helmet from 'react-helmet';
+import Layout from '../components/layout';
+import Banner from '../components/Banner';
 
 import Profile from '../components/profile';
 
 class HomeIndex extends React.Component {
-    render() {
+  render() {
+    return (
+      <Layout>
+        <Helmet
+          title="Gladino.net - The portfolio of Akash Pathak"
+          meta={[
+            { name: 'Gladino', content: 'Portfolio' },
+            {
+              name: 'google-site-verification',
+              content: '1ksHcltVQ1QGj1Ur9fHvluN5erMQ9NadoGj--xFY9YQ'
+            },
+            { name: 'keywords', content: 'akash, react, UI/UX, open source,portfolio ' }
+          ]}
+        ></Helmet>
 
-        return (
-            <Layout>
-                <Helmet
-                    title="Gladino.net - The portfolio of Akash Pathak"
-                    meta={[
-                        { name: 'Gladino', content: 'Portfolio' },
-                        { name: 'keywords', content: 'akash, react, UI/UX, open source,portfolio ' },
-                    ]}
-                >
-                </Helmet>
-
-                <Banner />
-                <Profile/>
-            </Layout>
-        )
-    }
+        <Banner />
+        <Profile />
+      </Layout>
+    );
+  }
 }
 
-export default HomeIndex
+export default HomeIndex;
